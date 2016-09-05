6ee6ac1cea7e7d964ce0724991bf411da0720056
out4b <seeds.c@gmail.com>
2016-09-05 11:38:53 +0800

    added partial support to generic ble device, actions needs to be added

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var util = require('util');
 var events = require('events');
 var NobleDevice = require('noble-device');
+var GenericDevice = require('./lib/generic-device');
 var supported_modules = require('./modules.json');
 
 var connect = function(user, pass, callback) {
+  var _this = this;
   this.device.connectAndSetup(function(err) {
+    if (!err) {
+      if (_this instanceof GenericDevice) {
+        _this.updateSpec();
+      }
+    }
     callback(err);
   });
 };
@@ -23,51 +30,21 @@ var getHWAddress = function(callback) {
   }
 };
 
-var collectDeviceInformation = function(device) {
-  device.device.readDeviceName(function(err, deviceName) {
-    if(!err) {
-      device.friendlyName = deviceName;
-    }
-  });
-  device.device.readSerialNumber(function(err, serialNumber) {
-    if(!err) {
-      device.serialNumber = serialNumber;
-    }
-  });
-  device.device.readFirmwareRevision(function(err, firmwareRevision) {
-    if(!err) {
-      device.firmwareRevision = firmwareRevision;
-    }
-  });
-  device.device.readHardwareRevision(function(err, hardwareRevision) {
-    if(!err) {
-      device.hardwareRevision = hardwareRevision;
-    }
-  });
-  device.device.readSoftwareRevision(function(err, softwareRevision) {
-    if(!err) {
-      device.softwareRevision = softwareRevision;
-    }
-  });
-  device.device.readManufacturerName(function(err, manufacturerName) {
-    if(!err) {
-      device.manufacturerName = manufacturerName;
-    }
-  });
-}
-
 function BleDeviceManager() {
   this.onDiscoverBleDevice = function(bleDevice) {
     var device = null;
     var peripheral = bleDevice._peripheral;
-    //TODO: in the future device type check should follow GATT standard profiles
-    supported_modules.forEach(function(item) {
-      var mod = require(item);
+
+    for (var i in supported_modules) {
+      var mod = require(supported_modules[i]);
       if (mod.is(peripheral)) {
         device = new mod(bleDevice);
+        break;
       }
-    });
-    if (device == null) return;
+    }
+    if (device === null) {
+      device = new GenericDevice(bleDevice);
+    }
     device._connect = connect.bind(device);
     device._disconnect = disconnect.bind(device);
     device._getHWAddress = getHWAddress.bind(device);
@@ -83,7 +60,6 @@ var BleDevice = function (peripheral) {
 }
 
 NobleDevice.Util.inherits(BleDevice, NobleDevice);
-NobleDevice.Util.mixin(BleDevice, NobleDevice.DeviceInformationService);
 
 BleDeviceManager.prototype.discoverDevices = function() {
   if (this.discoverState === 'discovering') {
